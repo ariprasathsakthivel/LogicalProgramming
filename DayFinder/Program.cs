2b7efab8574468c201f2bd8a12a61f373c3f11f7
ariprasathsakthivel <ariprasath3@gmail.com>
2021-07-09 08:18:44 +0530

    [Ariprasath] added: Find the day using date,month and year. Temperature conversion.

---
+++ DayFinder/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace DayFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the month number between 1 to 12");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the day");
+            int d = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the year in format YYYY");
+            int y = Convert.ToInt32(Console.ReadLine());
+            DayOfWeek(m, d, y);
+        }
+        static void DayOfWeek(int month, int day, int year)
+        {
+            int m = month, d = day, y = year;
+            int m1, d1, y1, x;
+            y1 = y - (14 - m) / 12;
+            x = y1 + (y1 / 4) - (y1 / 100) + (y1 / 400);
+            m1 = m + 12 * ((14 - m) / 12) - 2;
+            d1 = (d + x + 31 * m1 / 12) % 7;
+            toString(d1);
+        }
+        static void toString(int d1)
+        {
+            switch (d1)
+            {
+                case 0:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+            }
+        }
+    }
+}
---
+++ TemperatureConversion/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TemperatureConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TemperatureConversion();
+        }
+        static void TemperatureConversion()
+        {
+            Console.WriteLine("Enter the output temperature unit. Enter 'f' for Fahrenheit and 'c' for Celcius");
+            char key = Convert.ToChar(Console.ReadLine());
+            switch (key)
+            {
+                case 'f':
+                    Console.WriteLine("Enter the temperature in degreeCelcius. Limit should be between 0 and 100");
+                    int degC = Convert.ToInt32(Console.ReadLine());
+                    CelciusToFahrenheit(degC);
+                    break;
+                case 'c':
+                    Console.WriteLine("Enter the tempurature in degreeFahrenheit. Limit should be between 32 adn 212");
+                    int degF = Convert.ToInt32(Console.ReadLine());
+                    FahrenheitToCelcius(degF);
+                    break;
+            }
+        }
+        static void CelciusToFahrenheit(int degC)
+        {
+            int degF = (degC * 9 / 5) + 32;
+            Console.WriteLine(degF);
+        }
+        static void FahrenheitToCelcius(int degF)
+        {
+            int degC = (degF - 32) * 5 / 9;
+            Console.WriteLine(degC);
+        }
+    }
+}
