76156b06cb5cbcf8f78833e45ff2d84c2de84b8d
ariprasathsakthivel <ariprasath3@gmail.com>
2021-07-14 08:48:08 +0530

    [Ariprasath] added: finding the minimum notes for change in a vending machine

---
+++ VendingMachine/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace VendingMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the change amount");
+            int totalChange = Convert.ToInt32(Console.ReadLine());
+            Change(totalChange);
+        }
+        static void Change(int totalChange)
+        {
+            int[] notes = new int[] { 1000, 500, 100, 50, 10, 5, 2, 1 };
+            int[] noteCount = new int[7];
+            int j = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                if (totalChange >= notes[i])
+                {
+                    noteCount[i] = totalChange / notes[i];
+                    totalChange = totalChange - noteCount[i] * notes[i];
+                }
+
+            }
+            foreach (int count in noteCount)
+            {
+                Console.WriteLine(" Number of {0} are {1}", notes[j], count);
+                j++;
+            }
+        }
+    }
+}
